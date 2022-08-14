9437e7945430b08249271abf1ffdaef1688c0417
Karamwadhah <111229602+Karamwadhah@users.noreply.github.com>
2022-08-14 18:48:29 +0300

    Update Q1Write a method that will remove any given character from a String.py

--- Q1Write a method that will remove any given character from a String.py
+++ Q1Write a method that will remove any given character from a String.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 def remove_char(e1,e2):
     print(e1.replace(e2," "))
     
@@ -11,8 +5,6 @@ e1=input("Enter the string: ")
 e2=input("char to replace: ")
 remove_char(e1,e2)
 
-
-# In[ ]:
 
 
 
